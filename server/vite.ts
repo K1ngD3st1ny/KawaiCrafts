e7384f0884f143b05d283cab14a7b659e52dc3ef
K1ngD3st1ny <178103561+K1ngD3st1ny@users.noreply.github.com>
2026-02-17 00:22:00 +0530

    chore: bypass express.static to debug file serving

--- server/vite.ts
+++ server/vite.ts
@@ -76,7 +76,7 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  // app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
@@ -84,7 +84,7 @@ export function serveStatic(app: Express) {
     if (fs.existsSync(indexPath)) {
       res.sendFile(indexPath);
     } else {
-      res.status(500).type("text/plain").send(`
+      res.status(200).type("text/plain").send(`
         Error: Could not find index.html
         CWD: ${process.cwd()}
         DistPath: ${distPath}
