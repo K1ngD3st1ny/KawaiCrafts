166ce4fdd3a4aa3c9fd3e9bc68553ec622aef443
K1ngD3st1ny <178103561+K1ngD3st1ny@users.noreply.github.com>
2026-02-17 00:37:57 +0530

    fix: use dynamic import and correct includeFiles to unblock Vercel build

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
-import app from "../dist-server/index.js";
-
-export default app;
+export default async (req: any, res: any) => {
+    // Dynamic import to avoid build-time resolution error of missing artifact
+    // dist-server/index.js is created during build
+    const app = (await import("../dist-server/index.js")).default;
+    app(req, res);
+};
